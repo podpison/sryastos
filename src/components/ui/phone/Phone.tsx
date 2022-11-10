bd1b32e23d404da48d43b85f7e787e72fb5d6af8
podpison <au25557@yandex.ru>
2022-11-10 20:34:28 +0500

    0.2.1

--- src/components/ui/phone/Phone.tsx
+++ src/components/ui/phone/Phone.tsx
@@ -1,5 +1,5 @@
 import './phone.scss';
 
 export const Phone: React.FC = () => {
-  return <a className='phone' href='tel:+74959897723'>8 (495) 989 - 77 23</a>
+  return <a className='phone' href='tel:+74959897723'>+7 (495) 989 - 77 23</a>
 };
